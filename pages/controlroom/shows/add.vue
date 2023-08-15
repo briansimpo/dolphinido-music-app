4f41471bfad019060822a1f16b762fe2b15fac47
Brian Simpokolwe <90185393+luanardev@users.noreply.github.com>
2023-08-15 18:58:30 +0200

    Update add.vue

--- pages/controlroom/shows/add.vue
+++ pages/controlroom/shows/add.vue
@@ -12,7 +12,7 @@ definePageMeta({
         Add <span class="text-primary">Show</span>
       </SectionHead>
       <SectionBody>
-        <div class="col-lg-6 mx-auto">
+        <div class="col-lg-8 mx-auto">
           <div class="card">
             <div class="card-body">
               <AddShow />
