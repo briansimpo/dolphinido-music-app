af8aca5569b338d5356cf4324e01361196da461c
Brian Simpokolwe <90185393+luanardev@users.noreply.github.com>
2023-08-25 06:17:24 +0200

    Update ArtistMenu.vue

--- components/Menu/ArtistMenu.vue
+++ components/Menu/ArtistMenu.vue
@@ -2,10 +2,10 @@
   <nav class="navbar d-block p-0">
     <ul class="navbar-nav">
       <li class="nav-item">
-        <a href="" class="nav-link d-flex align-items-center">
+        <NuxtLink to="/controlroom/" class="nav-link d-flex align-items-center">
           <i class="ri-dashboard-line fs-5" />
           <span class="ps-3">Dashboard</span>
-        </a>
+        </NuxtLink>
       </li>
 
       <li class="nav-item nav-item--head">
