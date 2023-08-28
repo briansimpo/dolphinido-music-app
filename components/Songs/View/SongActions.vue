8765ea9482c58dc9b272133fbb820f23c4612290
Brian Simpokolwe <90185393+luanardev@users.noreply.github.com>
2023-08-28 07:22:05 +0200

    Update SongActions.vue

--- components/Songs/View/SongActions.vue
+++ components/Songs/View/SongActions.vue
@@ -34,7 +34,7 @@ const showUpdateDialog = ref(false)
       v-model:visible="showUpdateDialog"
       modal
       header="Update Song"
-      :style="{ width: '50vw' }"
+      :style="{ width: '40vw' }"
       :breakpoints="{ '960px': '75vw', '641px': '100vw' }"
     >
       <UpdateSong :song="props.song" />
