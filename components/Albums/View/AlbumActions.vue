9cef19de2992abe87b42ab09969b5c75052c6044
Brian Simpokolwe <90185393+luanardev@users.noreply.github.com>
2023-08-28 07:21:58 +0200

    Update AlbumActions.vue

--- components/Albums/View/AlbumActions.vue
+++ components/Albums/View/AlbumActions.vue
@@ -38,7 +38,7 @@ const showUpdateDialog = ref(false)
       v-model:visible="showUpdateDialog"
       modal
       header="Update Album"
-      :style="{ width: '50vw' }"
+      :style="{ width: '40vw' }"
       :breakpoints="{ '960px': '75vw', '641px': '100vw' }"
     >
       <UpdateAlbum :album="props.album" />
