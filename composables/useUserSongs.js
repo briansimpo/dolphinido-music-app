e39da6ad791c7b70b151ce16e5acdb32dd6be306
Brian Simpokolwe <90185393+luanardev@users.noreply.github.com>
2023-09-12 14:09:30 +0200

    Update useUserSongs.js

--- composables/useUserSongs.js
+++ composables/useUserSongs.js
@@ -10,9 +10,7 @@ export function useUserSongs () {
   })
 
   const songs = computed(() => {
-    if (data.value) {
-      return data.value.data
-    }
+    return data.value.data
   })
 
   return {
