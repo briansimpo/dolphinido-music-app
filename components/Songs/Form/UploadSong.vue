44338ff200238fa3415cd259972b1bd15b432c06
Brian Simpokolwe <90185393+luanardev@users.noreply.github.com>
2023-09-12 16:16:35 +0200

    Update UploadSong.vue

--- components/Songs/Form/UploadSong.vue
+++ components/Songs/Form/UploadSong.vue
@@ -16,39 +16,23 @@ function onDrop (acceptFiles) {
   }
 }
 
-function deleteFile () {
-  file.value = null
-}
 </script>
 
 <template>
   <div>
     <div v-if="file">
-      <div class="card">
-        <div class="card-header">
-          <button class="btn btn-danger" @click="deleteFile">
-            Cancel
-          </button>
-        </div>
-        <div class="card-body">
-          <CreateSong :file="file" />
-        </div>
-      </div>
+      <CreateSong :file="file" />
     </div>
     <div v-else v-bind="getRootProps()">
-      <div class="card">
-        <div class="card-body">
-          <FileDropzone>
-            <input accept="mp3" v-bind="getInputProps()">
-            <span class="d-block fs-6 mt-2 mb-2">
-              Drag & Drop or click to Upload
-            </span>
-            <button type="button" class="btn btn-light-primary">
-              Upload Song
-            </button>
-          </FileDropzone>
-        </div>
-      </div>
+      <FileDropzone>
+        <input accept="mp3" v-bind="getInputProps()">
+        <span class="d-block fs-6 mt-2 mb-2">
+          Drag & Drop or click to Upload
+        </span>
+        <button type="button" class="btn btn-light-primary">
+          Upload Song
+        </button>
+      </FileDropzone>
     </div>
   </div>
 </template>
