e9ed752e601433ed9e568f89b67a68177fc73ab4
Brian Simpokolwe <90185393+luanardev@users.noreply.github.com>
2023-10-10 21:32:13 +0200

    Update AlbumList.vue

--- components/Controlroom/Albums/List/AlbumList.vue
+++ components/Controlroom/Albums/List/AlbumList.vue
@@ -1,70 +1,84 @@
 <script setup>
-const config = useRuntimeConfig()
-const { token } = useAuthService()
-const { errorMessage } = useToastMessage()
-const page = ref(1)
-const limit = ref(10)
-const lastPage = ref(1)
-const datalist = ref([])
-const pending = ref(false)
+const { genres } = useGenres()
 
-onMounted(() => {
-  loadData()
+const params = ref({
+  genre: null,
+  sort: null,
+  page: 1,
+  limit: 10
 })
 
-const loadMore = () => {
-  if (page.value <= lastPage.value) {
-    page.value += 1
-    loadData()
-  }
-}
+const sortOptions = ref([
+  'title',
+  'year'
+])
 
-const loadData = async () => {
-  if (pending.value) {
-    return
-  }
-  pending.value = true
+const { pending, data, loadMore } = useDataFetch('/portal/albums', params.value)
 
-  await $fetch('/portal/albums', {
-    method: 'GET',
-    headers: {
-      Authorization: `Bearer ${token.value}`
-    },
-    baseURL: config.public.apiBase,
-    params: {
-      page: page.value,
-      limit: limit.value
-    }
-  }).then(function (response) {
-    appendData(response.data)
-    lastPage.value = response.meta.last_page
-  }).catch(function () {
-    errorMessage('Error loading data')
-  }).finally(function () {
-    pending.value = false
-  })
-}
-
-const appendData = (newData) => {
-  newData.forEach((data) => {
-    datalist.value.push(data)
-  })
+const redirect = () => {
+  return navigateTo('/controlroom/albums/add')
 }
 </script>
+
 <template>
-  <div>
-    <div class="list list--lg mt-5">
-      <div class="row">
-        <div v-if="pending">
-          <StateLoader />
+  <div class="list list--lg mt-5">
+    <div v-if="pending">
+      <StateLoader />
+    </div>
+    <div v-else>
+      <div class="row mb-6">
+        <div class="col-lg-6 mb-2">
+          <Button icon="pi pi-plus" label="Add" @click="redirect" />
         </div>
-        <div v-else>
-          <div v-for="album in datalist" :key="album.id" class="col-xl-12">
-            <AlbumListItem :album="album" class="mb-2" />
+        <div class="col-lg-6">
+          <div class="row">
+            <div class="col-lg-6 mb-2">
+              <div class="p-float-label">
+                <Dropdown
+                  v-model="params.genre"
+                  filter
+                  show-clear
+                  :options="genres"
+                  option-value="id"
+                  option-label="name"
+                  placeholder="Genre"
+                  class="w-full flex"
+                  required
+                />
+                <label for="dd-city">Genre</label>
+              </div>
+            </div>
+            <div class="col-lg-6">
+              <div class="p-float-label">
+                <Dropdown
+                  v-model="params.sort"
+                  show-clear
+                  :options="sortOptions"
+                  placeholder="Sort By"
+                  class="w-full flex"
+                  required
+                />
+                <label for="dd-city">Sort By</label>
+              </div>
+            </div>
           </div>
         </div>
-        <div class="mt-5 text-center">
-          <LoadMoreButton @click="loadMore" />
+      </div>
+      <div v-if="data.length > 0" class="row">
+        <div class="col-lg-12">
+          <div v-for="album in data" :key="album.id">
+            <AlbumListItem :album="album" class="mb-2" />
+          </div>
+          <div class="mt-5 text-center">
+            <LoadMoreButton @click="loadMore" />
+          </div>
+        </div>
+      </div>
+      <div v-else class="row">
+        <div class="text-center mb-4">
+          <p class="display-5 email">
+            You do not have any albums
+          </p>
         </div>
       </div>
     </div>
